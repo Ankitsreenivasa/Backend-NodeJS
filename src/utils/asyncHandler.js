9b76333d98c4aeb7f33fb1d7cfc3cdcda1d6d41b
Ankitsreenivasa <ankit.sreeni@gmail.com>
2024-01-16 22:03:28 +0530

    api Error handeling

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+const asyncHandler = (requestHandeler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandeler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHanler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
